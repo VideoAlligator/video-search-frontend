829a3ef121848430a240d88468a8b160e23b7d14
yixuannliu <yixuann.liu.utoronto@gmail.com>
2020-11-09 16:13:38 -0500

    prettier, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import Button from '@material-ui/core/Button'
 
-function App() {
-    return (
-        <div>
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
-            <p>Yes, I'm here.</p>
-        </div>
-    );
+function App(): JSX.Element {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      <p>Yes, I'm here.</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
