1128ed2c160fbf8c9532ee3aaa6712c4b0952f4d
moranxuanqing <1751506212@qq.com>
2021-01-09 12:05:18 +0800

    change keywords and genres in cardinfo

--- src/common/components/molecules/VideoInfoCard/index.tsx
+++ src/common/components/molecules/VideoInfoCard/index.tsx
@@ -6,6 +6,7 @@ import { Video } from 'common/types/video'
 import { Text, TypographyStyles } from 'common/components/atoms/Typography'
 
 import { StyledCard } from './styled'
+import Chip from '@material-ui/core/Chip'
 
 interface VideoInfoCardProps {
   video: Video
@@ -21,32 +22,42 @@ export const VideoInfoCard: FC<VideoInfoCardProps> = ({ video }) => {
           <Box p={1} />
           <Text>{title}</Text>
         </Box>
-        <Box display="flex">
+        <Box mt={1} display="flex">
           <Text type={TypographyStyles.labelCopy}>Duration: </Text>
           <Box p={1} />
           <Text>{duration}</Text>
         </Box>
         {keywords.length > 0 && (
-          <>
-            <Text type={TypographyStyles.labelCopy}>Keywords: </Text>
-            {keywords.map((keyword, index) => (
-              <div key={index}>
-                <Text>{keyword}</Text>
-              </div>
-            ))}
-          </>
+          <Box mt={1} display="flex">
+            <>
+              <Text type={TypographyStyles.labelCopy}>Keywords: </Text>
+              {keywords.map((keyword, index) => (
+                <div key={index}>
+                  <Chip style={{ marginLeft: 10 }} label={keyword} />
+                  {/* <Text>{keyword}</Text> */}
+                </div>
+              ))}
+            </>
+          </Box>
         )}
         {genres.length > 0 && (
-          <>
-            <Text type={TypographyStyles.labelCopy}>Genres: </Text>
-            {genres.map((genre, index) => (
-              <div key={index}>
-                <Text>{genre}</Text>
-              </div>
-            ))}
-          </>
+          <Box mt={1} display="flex">
+            <>
+              <Text type={TypographyStyles.labelCopy}>Genres: </Text>
+              {genres.map((genre, index) => (
+                <div key={index}>
+                  <Chip
+                    style={{ marginLeft: 10 }}
+                    label={genre}
+                    color="secondary"
+                  />
+                  {/* <Text>{genre}</Text> */}
+                </div>
+              ))}
+            </>
+          </Box>
         )}
-        <Box display="flex">
+        <Box mt={1} display="flex">
           <Text type={TypographyStyles.labelCopy}>Overview: </Text>
           <Box p={1} />
           <Text>{overview}</Text>
--- src/common/style/theme.ts
+++ src/common/style/theme.ts
@@ -6,7 +6,7 @@ export const theme: any = {
     borderSeparator: '#ecebeb',
     mainColor: '#D63649',
     textPrimary: '#252525',
-    lightBody: 'rgba(0, 0, 0, 0.54)',
+    lightBody: '#b7e0f5',
     iconDefault: '#DADADA',
     lightBackground: '#f6f6f7',
     mainBackground: '#fafafa',
