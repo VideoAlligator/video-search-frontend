c940805be9f9629af087a8d07e6bcd3917f653d0
yixuannliu <yixuann.liu.utoronto@gmail.com>
2021-01-09 14:55:01 +0800

    cleanup

--- src/common/components/templates/PageLayout/styled.ts
+++ src/common/components/templates/PageLayout/styled.ts
@@ -2,8 +2,6 @@ import styled from 'styled-components'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 
-import { muiTheme } from 'common/style/theme'
-
 export const StyledGridContainer = styled(Grid)`
   && {
     flex-wrap: nowrap;
@@ -20,12 +18,6 @@ export const PageGridItem = styled(Grid).attrs({
   align-items: stretch;
 `
 
-export const FooterContainer = styled.div`
-  padding: 30px;
-  background: ${({ theme }) => theme.colors.textPrimary};
-  color: white;
-`
-
 export const StyledContentContainer = styled(Container).attrs({
   maxWidth: 'xl',
 })`
@@ -36,17 +28,5 @@ export const StyledContentContainer = styled(Container).attrs({
 export const FullHeightGrid = styled(Grid)`
   && {
     height: 100%;
-  }
-`
-
-export const TopBarContainer = styled.div`
-  padding: 16px 30px;
-  display: flex;
-  align-items: center;
-  background: white;
-  box-shadow: 0 1px 10px 0 rgba(0, 0, 0, 0.05);
-
-  @media screen and (max-width: ${muiTheme.breakpoints.values.sm}px) {
-    padding: 16px;
   }
 `
