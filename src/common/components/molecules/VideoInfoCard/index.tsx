117f2a0cd704b4c517b07b82bcc2efd360b21e92
yixuannliu <yixuann.liu.utoronto@gmail.com>
2021-01-10 18:27:40 +0800

    timestamps

--- src/common/components/molecules/VideoInfoCard/index.tsx
+++ src/common/components/molecules/VideoInfoCard/index.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from 'react'
 import Box from '@material-ui/core/Box'
-import CardContent from '@material-ui/core/CardContent'
+import { CardContent, CardActionArea, CardMedia } from '@material-ui/core'
+import Chip from '@material-ui/core/Chip'
 
 import { Video } from 'common/types/video'
 import { Text, TypographyStyles } from 'common/components/atoms/Typography'
 
 import { StyledCard } from './styled'
-import { CardActionArea, CardMedia } from '@material-ui/core'
 
 interface VideoInfoCardProps {
   video: Video
 }
 
 export const VideoInfoCard: FC<VideoInfoCardProps> = ({ video }) => {
-  const { _id, title, duration, posterUrl } = video
+  const { _id, title, duration, posterUrl, details } = video
   return (
     <StyledCard>
       <CardActionArea href={'/video/' + _id}>
@@ -26,12 +26,24 @@ export const VideoInfoCard: FC<VideoInfoCardProps> = ({ video }) => {
           />
           <CardContent>
             <Box display="flex">
-              <Text type={TypographyStyles.movieName}>{title} </Text>
+              <Text type={TypographyStyles.primaryHeadline}>{title}</Text>
             </Box>
             <Box mt={1} display="flex">
-              <Text type={TypographyStyles.labelCopy}>Duration: </Text>
+              <Text type={TypographyStyles.labelCopy}>Duration:</Text>
               <Box p={1} />
               <Text>{duration}</Text>
+            </Box>
+            <Box mt={1} display="flex" alignItems="center">
+              <Text type={TypographyStyles.labelCopy}>Timestamps:</Text>
+              <Box p={1} />
+              <Box display="flex">
+                {details.map((detail, index) => (
+                  <Box key={index} mr={2} display="flex" alignItems="center">
+                    <Chip style={{ marginRight: 10 }} label={detail.keyword} />
+                    <Text> {detail.timestamp} seconds</Text>
+                  </Box>
+                ))}
+              </Box>
             </Box>
           </CardContent>
         </Box>
--- src/core/VideoDetailsPage/VideoDetails.tsx
+++ src/core/VideoDetailsPage/VideoDetails.tsx
@@ -1,16 +1,24 @@
 import React, { FC } from 'react'
 import Box from '@material-ui/core/Box'
+import Chip from '@material-ui/core/Chip'
 
 import { Video } from 'common/types/video'
 import { Text, TypographyStyles } from 'common/components/atoms/Typography'
-import Chip from '@material-ui/core/Chip'
 
 interface VideoDetailsProps {
   video: Video
 }
 
 export const VideoDetails: FC<VideoDetailsProps> = ({ video }) => {
-  const { title, duration, keywords, genres, overview, posterUrl } = video
+  const {
+    title,
+    duration,
+    keywords,
+    genres,
+    overview,
+    posterUrl,
+    details,
+  } = video
   return (
     <Box display="flex">
       <img height={300} width={200} src={posterUrl} alt={title} />
@@ -20,14 +28,14 @@ export const VideoDetails: FC<VideoDetailsProps> = ({ video }) => {
             back
           </a>
         </Box>
-        <Text type={TypographyStyles.movieName}>{title} </Text>
+        <Text type={TypographyStyles.primaryHeadline}>{title}</Text>
         <Box mt={1} display="flex">
           <Text type={TypographyStyles.labelCopy}>Duration: </Text>
           <Box p={1} />
           <Text>{duration}</Text>
         </Box>
         {keywords.length > 0 && (
-          <Box mt={1} display="flex">
+          <Box mt={1} display="flex" alignItems="center">
             <>
               <Text type={TypographyStyles.labelCopy}>Keywords: </Text>
               {keywords.map((keyword, index) => (
@@ -39,7 +47,7 @@ export const VideoDetails: FC<VideoDetailsProps> = ({ video }) => {
           </Box>
         )}
         {genres.length > 0 && (
-          <Box mt={1} display="flex">
+          <Box mt={1} display="flex" alignItems="center">
             <>
               <Text type={TypographyStyles.labelCopy}>Genres: </Text>
               {genres.map((genre, index) => (
@@ -58,6 +66,18 @@ export const VideoDetails: FC<VideoDetailsProps> = ({ video }) => {
           <Text type={TypographyStyles.labelCopy}>Overview: </Text>
           <Box p={1} />
           <Text>{overview}</Text>
+        </Box>
+
+        <Box mt={3} mb={1}>
+          <Text type={TypographyStyles.primaryHeadline}>Timestamps</Text>
+        </Box>
+        <Box display="flex" flexDirection="column">
+          {details.map((detail, index) => (
+            <Box key={index} mr={2} mb={2} display="flex" alignItems="center">
+              <Chip style={{ marginRight: 10 }} label={detail.keyword} />
+              <Text> {detail.timestamp} seconds</Text>
+            </Box>
+          ))}
         </Box>
       </Box>
     </Box>
